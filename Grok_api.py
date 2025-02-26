7b7598c3de5826e93f2bb0af7ab1d3accb838697
Avox-dev <gnfmcm333@naver.com>
2025-02-26 12:51:16 +0900

    Update model in Grok_req function to use 'llama-3.3-70b-versatile' for improved performance

--- Grok_api.py
+++ Grok_api.py
@@ -5,7 +5,7 @@ def Grok_req(prompt: str,api_key) -> str:
 
     client = Groq(api_key=api_key)
     completion = client.chat.completions.create(
-        model="llama-3.3-70b-specdec",
+        model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": prompt}],
         temperature=1,
         max_completion_tokens=1024,
